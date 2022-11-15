7338477ba398492a1a1acf047a9583a1bca2b585
Caleb Street <64947103+Streetcr@users.noreply.github.com>
2022-11-15 15:16:59 -0500

    Added basic requirements for project

--- Client-Server/client.cc
+++ Client-Server/client.cc
@@ -8,6 +8,9 @@
 #include <iostream>
 #include <string.h>
 #include <strings.h>
+#include <bits/stdc++.h>
+#include <pthread.h>
+
 using namespace std;
 
 // Message record
@@ -15,23 +18,59 @@ struct message {
    int    ivalue;
    double dvalue;
    char   cvalue[56];
+   char   mname[21];
 };
+
+int connection;
+message incmessage;
+char name[21];
+
+void* listener(void* arg)
+   {
+      pthread_detach(pthread_self());
+      int* temp = (int*)arg;
+      int connection = *temp;
+
+      for(;;)
+      {
+         read(connection, (char*)&incmessage, sizeof(message));
+         char sender[21];
+         if(strcmp(incmessage.mname, name) == 0)
+         {
+            strcpy(sender, "\033[1;32mYou\033[0m");
+
+            if(strcmp(incmessage.cvalue, "bye") == 0)
+            {
+               break;
+            }
+         }
+         else{
+            strcpy(sender, incmessage.mname);
+         }
+         cout << "\033[1;31m" << sender << "\033[0m: " << incmessage.cvalue << endl;
+      }
+
+      //pthread_exit(NULL);
+      return 0;
+   }
 
 int main(int argc, char** argv)
 {
    struct addrinfo *myinfo; // Address record
-   int sockdesc;
    char hostname[81];
    char portnum[81];
    message mymessage;
-   int connection;
+   int sockdesc;
    int value;
+   char chat[56];
+   pthread_t ptid;
 
    if ( argc == 1 )
    {
       cout << "Usage: client <host> [<portnum>]" << endl;
       exit(0);
    }
+
    strcpy(hostname, argv[1]);
    if ( argc == 3 )
    {
@@ -42,9 +81,10 @@ int main(int argc, char** argv)
       strcpy(portnum, "2000");
    }
 
+   cout << "Attempting to connect..." << endl;
+
    // Use AF_UNIX for unix pathnames instead
    // Use SOCK_DGRAM for UDP datagrams
-   cout << "Before socket" << endl;
    sockdesc = socket(AF_INET, SOCK_STREAM, 0);
    if ( sockdesc < 0 )
    {
@@ -53,7 +93,6 @@ int main(int argc, char** argv)
    }
 
    // Set up the address record
-   cout << "Before getaddrinfo" << endl;
    if ( getaddrinfo(hostname, portnum, NULL, &myinfo) != 0 )
    {
       cout << "Error getting address" << endl;
@@ -61,40 +100,42 @@ int main(int argc, char** argv)
    }
 
    // Connect to the host
-   cout << "Before connect" << endl;
    connection = connect(sockdesc, myinfo->ai_addr, myinfo->ai_addrlen);
    if ( connection < 0 )
    {
       cout << "Error in connect" << endl;
       exit(0);
    }
-   cout << "Client connection = " << connection << endl;
+   cout << "Connected!" << endl;
 
-   mymessage.ivalue = 1;
-   mymessage.dvalue = 34.2;
-   strcpy(mymessage.cvalue, "Client message");
+   cout << "Enter your chat name: " << endl;
+   cin.getline(name, 20);
 
-   // Display the message to be sent
-   cout << "Client sends: " << endl;
-   cout << "  ivalue: " << mymessage.ivalue << endl;
-   cout << "  dvalue: " << mymessage.dvalue << endl;
-   cout << "  cvalue: " << mymessage.cvalue << endl;
+   if(pthread_create(&ptid, NULL, &listener, (void*)&sockdesc))
+   {
+      cout << "Thread creation failed!" << endl;
+   }
 
-   // Send the message to the server
-   write(sockdesc, (char*)&mymessage, sizeof(message));
-
-   // Read back exactly one message
-   value = read(sockdesc, (char*)&mymessage, sizeof(message));
-
-   // Display the received message
-   cout << "Client gets back: " << endl;
-   cout << "  ivalue: " << mymessage.ivalue << endl;
-   cout << "  dvalue: " << mymessage.dvalue << endl;
-   cout << "  cvalue: " << mymessage.cvalue << endl;
-
-   // Close the socket
-   close(sockdesc);
-
+   cout << "Begin chatting: " << endl;
+   for(;;)
+   {
+      mymessage.ivalue = 1;
+      mymessage.dvalue = 32.4;
+      memcpy(mymessage.mname, name, sizeof(mymessage.mname));
+      cin.getline(chat, 55);
+      strcpy(mymessage.cvalue, chat);
+        
+      write(sockdesc, (char*)&mymessage, sizeof(message));
+      
+      
+      if(strcmp(chat, "bye") == 0)
+      {
+         break;
+      }
+   }
+      // Close the socket
+      pthread_join(ptid, NULL);
+      close(sockdesc);
    return 0;
 
 }
